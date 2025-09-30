946e1f98df0ef9a3d2cc919ef3909007c1ae4072
Brunna <brunna.louisemr@gmail.com>
2025-09-30 20:21:29 -0300

    fix: corrige conftest para suportar testes ci workflows

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
@@ -8,11 +9,21 @@ from app.main import app
 from app.db.session import Base
 from app.db.dependencies import get_db
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+TEST_DATABASE_URL = os.environ.get("FAKE_DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, poolclass=StaticPool)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+if TEST_DATABASE_URL:
+    print("--- Usando Banco de Dados PostgreSQL para testes ---")
+    engine = create_engine(TEST_DATABASE_URL)
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+else:
+    print("--- Usando Banco de Dados SQLite em memória para testes ---")
+    SQLALCHEMY_DATABASE_URL_LOCAL = "sqlite:///:memory:"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL_LOCAL,
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def override_get_db():
     db = TestingSessionLocal()
