a4c0d330abd7ebb75a2f752371945bbae25129c5
Brunna <brunna.louisemr@gmail.com>
2025-11-21 19:38:15 -0300

    fix: corrige var origins

--- src/main.py
+++ src/main.py
@@ -45,15 +45,11 @@ def create_app() -> FastAPI:
         lifespan=lifespan,
     )
 
-    origins = [
-        "http://localhost",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ]
+    origins = ["*"]
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
